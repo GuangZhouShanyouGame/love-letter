6d3b59bdc59a2475884b69c5deec06d27ac059fc
songyuzhou <2877293570@qq.com>
2018-05-17 15:59:49 +0800

    up

---
+++ src/views/browse/browse.ts
@@ -0,0 +1,11 @@
+
+import Vue from 'components/base'
+import { Component } from 'vue-property-decorator'
+import template from './browse.vue'
+
+@Component({
+  mixins: [template]
+})
+export default class Browse extends Vue {
+
+}
---
+++ src/views/browse/browse.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Browse</h1>
+  </div>
+</template>
+<style src="./browse.scss" lang="scss" scoped></style>
