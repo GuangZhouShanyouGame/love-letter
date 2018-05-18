aad968c2bc0cab38d90ea0f53f0fe2649cdf0f9e
weijianghong <jianghong.wei@24haowan.com>
2018-05-18 11:48:31 +0800

    推送

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import Vue from 'vue'
-
 import store from 'store'
 import App from './App.vue'
 import router from 'router'
 import svgicon = require('vue-svgicon')
-import hwlever from 'hwlever';
+
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 
@@ -31,26 +30,20 @@ import 'components/icons'
 
 //   document.documentElement.appendChild(script);
 // };
-
 // loadScript('http://24haowan-cdn.shanyougame.com/public/js/vconsole.min.js')
-
-// alloylever.config({
-//   cdn: '//24haowan-cdn.shanyougame.com/public/js/vconsole.min.js',
-// })
-// hwlever.config({
-//   cdn: '//24haowan-cdn.shanyougame.com/public/js/vconsole.min.js',
-//   entry: 'EasterEgg',
-//   vueObj: Vue,
-//   debug: process.env.NODE_ENV === 'development',
-//   ravenId: '//56d67d26f9854c21a1f8e7b83854fecd@sentry.24haowan.com/12',
-//   // extraConf: {
-//   // 	tags: {
-//   // 		from: 'hwlever hilton'
-//   // 	}
-//   // },
-//   type: []
-// });
-console.log(hwlever)
+var AlloyLever = window.AlloyLever || false
+if(!!AlloyLever) {
+  AlloyLever.config({
+    cdn: '//24haowan-cdn.shanyougame.com/public/js/vconsole.min.js',  //vconsole的CDN地址
+    reportUrl: "//a.qq.com",  //错误上报地址
+    reportPrefix: 'qun',    //错误上报msg前缀，一般用于标识业务类型
+    reportKey: 'msg',        //错误上报msg前缀的key，用户上报系统接收存储msg
+    otherReport: {              //需要上报的其他信息
+      uin: 491862102
+    },
+    entry: "#entry"          //请点击这个DOM元素6次召唤vConsole。//你可以通过AlloyLever.entry('#entry2')设置多个机关入口召唤神龙
+  })
+}
 var _hmt = _hmt || [];
 (function () {
   var hm = document.createElement("script");
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,6 +1,7 @@
 export default {
     handleInception() {
-        if (!window.vConsole) {
+        const date = new Date('2018/05/19 09:00:00').getTime()
+        if (!window.vConsole && (new Date().getTime() < date)) {
             this.$router.replace('/interception')
         }
     }
