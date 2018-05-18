303ba835882a0f35f3ff6f8b5969940fda1b729e
weijianghong <jianghong.wei@24haowan.com>
2018-05-18 15:55:56 +0800

    切换环境

--- src/env/dev.ts
+++ src/env/dev.ts
@@ -1,4 +1,5 @@
 export default {
   env: 'dev',
-  baseURL: 'http://test.c56e37f627a5a42e89b31387f12f59fab.cn-shenzhen.alicontainer.com', // api server baseURL
+  // baseURL: 'http://test.c56e37f627a5a42e89b31387f12f59fab.cn-shenzhen.alicontainer.com', // api server baseURL
+  baseURL: 'http://custom.c56e37f627a5a42e89b31387f12f59fab.cn-shenzhen.alicontainer.com'
 }
